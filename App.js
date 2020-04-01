4c888b4cdbbe7c9cfbe57b83909de3c3f15c5fbf
navdeepdadhania1852 <54733487+navdeepdadhania1852@users.noreply.github.com>
2020-04-01 18:44:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,47 @@
 import React from 'react';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import {createAppContainer,StackNavigator} from 'react-navigation';
+import { createAppContainer, StackNavigator } from 'react-navigation';
 
-import SmartHome from './navigation/SmartHome';
+import SmartHomes_Distributor from './navigation/SmartHome_Distributor';
+import SmartHomes_Manufacturer from './navigation/SmartHome_Manufacturer';
+import SmartHomes_Shopkeeper from './navigation/SmartHome_Shopkeeper';
+import Login from './screens/Login';
 
 
 
 
 const AppNavigator = createStackNavigator({
- 
- SmartHomes: {
-   screen:SmartHome,
-    navigationOptions:{
-   header: null
- },
+
+  Login: {
+    screen: Login,
+    navigationOptions: {
+      header: null
+    },
+  },
+  SmartHomes_Manufacturer: {
+    screen: SmartHomes_Manufacturer,
+    navigationOptions: {
+      header: null
+    },
+  },
+  SmartHomes_Distributor: {
+    screen: SmartHomes_Distributor,
+    navigationOptions: {
+      header: null
+    },
+  },
+  SmartHomes_Shopkeeper: {
+    screen: SmartHomes_Shopkeeper,
+    navigationOptions: {
+      header: null
+    },
+  },
 },
-})
-
- //const MainNavigation = StackNavigator({
-  //headerMode: 'none',
-  //navigationOptions: {
-    //headerVisible: false,
-  //}
- //}
-//);
-
-
+  {
+    initialRouteName: 'Login',
+  },
+)
 
 const AppContainer = createAppContainer(AppNavigator);
 
